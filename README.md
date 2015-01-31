608304e018fc102799d6d91bffdf84767402ac2a
Shinobu Kawano <kawanoshinobu@gmail.com>
2015-01-31 22:22:12 +0900

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # chrome-talk
-Simple video chat application for Google Chrome
+
+Simple video chat application for Google Chrome.
+
+Feature
+=======
+
+[TODO]
+
+Installation
+=======
+
+[TODO]
+
+Author
+=======
+* [KAWANO Shinobu](https://github.com/kawanoshinobu) - [http://kawanoshinobu.com](http://kawanoshinobu.com)
+
+License
+=======
+Licensed under the GPL v3 License.
+
--- app/util/Consts.js
+++ app/util/Consts.js
@@ -2,11 +2,11 @@
  *
  */
 Ext.define('CT.util.Consts', {
-    alternateClassName : 'CT.Consts',
-    singleton : true,
+    alternateClassName     : 'CT.Consts',
+    singleton              : true,
 
-    APP_TITLE : 'CHROME TALK <i class="fa fa-comments"></i>',
-    BUTTON_CONNECT_TEXT : '<i class="fa fa-video-camera"></i> CONNECT',
+    APP_TITLE              : 'CHROME TALK <i class="fa fa-comments"></i>',
+    BUTTON_CONNECT_TEXT    : '<i class="fa fa-video-camera"></i> CONNECT',
     BUTTON_DISCONNECT_TEXT : '<i class="fa fa-stop"></i> DISCONNECT'
 
 });
